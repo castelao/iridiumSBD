431a12163709b90dd2f57958744fcf0b4bac3308
Guilherme Castelão <guilherme@castelao.net>
2019-07-08 16:29:48 -0700

    Bump version: 0.0.12 → 0.0.13

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='iridiumSBD',
-    version='0.0.12',
+    version='0.0.13',
     description="Communication system for Iridium Short Burst Data Service.",
     long_description=readme + '\n\n' + history,
     author="Guilherme Castelão",
